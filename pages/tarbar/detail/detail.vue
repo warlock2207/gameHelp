12b918a05a7f71638feb2d3974ad2e1b1113684d
warlock2207 <1>
2023-04-23 22:36:42 +0800

    111

--- pages/tarbar/detail/detail.vue
+++ pages/tarbar/detail/detail.vue
@@ -4,25 +4,46 @@
 		pillsBorderRadius="50rpx">
 	</v-tabs>
 		<view><carousel :img-list="imgList" url-key="url" @selected="selectedBanner"/></view>
-		  <view class="example-body">
-			  <view>
-			  	<text> <font style="font-size:45rpx;font-family:'黑体';font-weight: bold;">泰拉瑞亚</font> </text>
-			  </view>
-			  <view>
-			  	trrair
-			  </view>
-			  	<view class="ktetx"><font style="color: #ff0000;font-size:20rpx;align-items:center">游戏</font>
-			  	<font style="color: #797979;">2018-11-06/325M</font>
-			  	</text>
-				</view>
-				<view class="box_right">
-					 <image class="grade" src="/static/icon/hexagon.png"></image>
-					 <text class = "number">2.0</text>
-				</view>
-			</view>
-			<view>
+				<view class="example-body">
+							   <view>
+									<text> <font style="font-size:45rpx;font-family:'黑体';font-weight: bold;">泰拉瑞亚</font> </text>
+									<view>
+										trrair
+									</view>
+									
+								</view>
+								<view class="box_right">
+									 <image class="grade" src="/static/icon/hexagon.png"></image>
+									 <text class = "number">2.0</text>
+								</view>
+							</view>
+							<view class="example-body2">
+								<view class="ktetx">
+								<font class="titletx">游戏</font>
+								</view>
+								<view class="otherInfo">
+									<font style="color: #797979;font-size:22rpx">2018-11-06/多人游玩/325M</font>
+								</view>
+							</view>
+							<view class="example-body2">
+									 <image class="lable-grade" src="/static/icon/label_image.png"></image>
+									 <view class="label-info">
+										 竞速
+									 </view>
+									 <view class="label-info">
+									 	多人
+									 </view>
+									 <view class="label-info">
+									 	其他
+									 </view>
+							</view>
+							<view class="info-all">
+								
+							</view>
+		  
+			<!-- <view>
 				<text class="uni-text">uni-app是一个使用Vue.js开发所有前端应用的框架，开发者编写一套代码，可发布到iOS、Android、H5、以及各种小程序（微信/支付宝/百度/头条/QQ/钉钉）等多个平台。即使不跨端，uni-app同时也是更好的小程序开发框架。DCloud公司拥有370万开发者用户，旗下uni-app有5万+案例、900款插件、50+微信/qq群，并且被阿里小程序工具内置，开发者可以放心选择。</text>
-			</view>	  
+			</view>	  -->
 	</view>
 </template>
 
@@ -62,13 +83,19 @@
 
 <style lang="scss">
 	.example-body {
-		/* #ifndef APP-NVUE */
-		/* #endif */
-		padding: 7px;
+		padding: 5px;
 		left: 16upx;
 		display: inline; 
 		display: flex;
 		justify-content: space-between;
+		align-items: center;
+		padding-right: 12px;
+	}
+	.example-body2 {
+		padding: 5px;
+		left: 16upx;
+		display: flex;
+		flex-direction: row;
 		align-items: center;
 		padding-right: 12px;
 	}
@@ -105,12 +132,61 @@
 		height: 35px;
 		text-align: center;
 	}
+	.lable-grade{
+		width: 30px;
+		height: 22px;
+		text-align: center;
+	}
+	
+	
 	 .ktetx {
 	    border: 1.2px solid red;
-	     height: 15px;
-	     width: 25px;
+	     height: 18px;
+	     width: 34px;
 	    border-radius: 10px;
 		align-items:center;
 		justify-content: center;
 	 }
+	 .label-info {
+		border: 1.1px;
+	    background-color: #EEEEEE;
+	    height: 18px;
+	    width: 34px;
+	    border-radius: 5px;
+		font-size: 18rpx;
+		align-items:center;
+		display: flex;
+		align-items: center;
+		vertical-align: center;
+		justify-content: center;
+		flex-direction: row;
+		margin-left: 4px;
+	 }
+	 .titletx{
+		 color: #ff0000;
+		 font-size:18rpx;
+		 align-items:center;
+		 display: flex;
+		 align-items: center;
+		 vertical-align: center;
+		 justify-content: center;
+		 flex-direction: row;
+	 }
+	 .otherInfo{
+	 	margin-left: 10rpx;
+		align-items: center;
+		display:block;
+	 }
+	.info-all{
+		background-color: #EEEEEE;
+		height: 50px;
+		width: 350px;
+		border-radius: 5px;
+		left: 16upx;
+	}
+	.info-all{
+		background-color: #EEEEEE;
+		padding-left: 5px;
+		padding-right: 5px;
+	}
 </style>
